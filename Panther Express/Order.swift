9b185d3ba4709d73f630a02c19606504fe620b3a
Christian Valencia <christianvalenciav@gmail.com>
2020-07-18 14:13:02 -0400

    Implemented Firebase

---
+++ Panther Express/Order.swift
@@ -0,0 +1,21 @@
+//
+//  Order.swift
+//  Panther Express
+//
+//  Created by Christian Valencia on 7/18/20.
+//  Copyright © 2020 PantherHacks. All rights reserved.
+//
+
+import Foundation
+
+class Order: Codable {
+    var requestedBy = User()
+    var excecutedBy = User()
+    var date = Date()
+    var deliverySummary = ""
+    var excecuterRating = -1
+    var requestRating = -1
+    var open = false
+    var inProgress = false
+    var completead = false
+}
